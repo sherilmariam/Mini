c0c7f89209cb8d04d213b97d3157b70afe9d7ea0
Sheril <sherilmariamjose@gmail.com>
2024-07-06 14:03:03 +0530

    deleted

--- price-palate/price-palate/Backend/Backend/src/routes/restaurant.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const restaurantController = require('../controllers/restaurant.controller');
-
-router.get('/restaurants', restaurantController.getAllRestaurants);
-router.get('/restaurants/:name/dishes', restaurantController.getRestaurantDishes);
-
-module.exports = router;
